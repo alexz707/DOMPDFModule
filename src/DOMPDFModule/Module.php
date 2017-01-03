94f7a3245a695b627aeb784b6db04960d3893c88
Michał Makaruk <buliq1847@gmail.com>
2017-01-03 07:32:19 +0100

    fixed for zf3

--- src/DOMPDFModule/Module.php
+++ src/DOMPDFModule/Module.php
@@ -28,15 +28,4 @@ class Module
     {
         return include __DIR__ . '/../../config/module.config.php';
     }
-    
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ ,
-                ),
-            ),
-        );
-    }
 }
