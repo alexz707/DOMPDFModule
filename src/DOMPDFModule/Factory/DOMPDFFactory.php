caad426998c324d7599988cb29afec63b29d625f
Alex Zamponi <aza@php-unlimited.at>
2017-01-14 13:42:16 +0100

    Update DOMPDFFactory.php

--- src/DOMPDFModule/Factory/DOMPDFFactory.php
+++ src/DOMPDFModule/Factory/DOMPDFFactory.php
@@ -89,7 +89,7 @@ class DOMPDFFactory implements FactoryInterface
         $this->applyCompatGlobals($config['dompdf_module']);
 
         require_once DOMPDF_LIB_DIR . '/html5lib/Parser.php';
-        require_once DOMPDF_INC_DIR . '/functions.inc.php';
+        //require_once DOMPDF_INC_DIR . '/functions.inc.php';
         require_once __DIR__ . '/../../../config/module.compat.php';
 
         return new Dompdf();
